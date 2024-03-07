717cede5f2fa8aeefc7bd7cd53c12f9795a5aaf8
Radmir Musabaev <musabaevradmir@gmail.com>
2024-03-07 23:05:18 +0400

    feat: added initial states for context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-import { SearchForm } from "./components/SearchFrom/SearchForm";
-import { SearchContext } from "./components/SearchResults/SearchContext";
-import { SearchResults } from "./components/SearchResults/SearchResults";
-import { mockUsers } from "./mockUsers";
+import {useState} from "react";
+import {SearchForm} from "./components/SearchFrom/SearchForm";
+import {SearchContext} from "./components/SearchResults/SearchContext";
+import {SearchResults} from "./components/SearchResults/SearchResults";
+import {ResponseEntity} from "./components/types/ResponseEntity.ts";
 
 export default function App() {
-  const [users] = useState(mockUsers);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState<ResponseEntity>({limit: 0, skip: 0, total: 0, users: []});
 
-  return (
-    <SearchContext.Provider value={{ users }}>
-      <SearchForm />
-      <SearchResults />
-    </SearchContext.Provider>
-  );
+    return (
+        <SearchContext.Provider value={{searchTerm, setSearchTerm, searchResults, setSearchResults}}>
+            <SearchForm/>
+            <SearchResults/>
+        </SearchContext.Provider>
+    );
 }
