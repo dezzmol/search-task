3f1f47a230b27802fc4ebeb240c8c7da289ecfba
Radmir Musabaev <musabaevradmir@gmail.com>
2024-03-07 23:03:00 +0400

    feat: added fetching data from api

--- src/components/SearchFrom/SearchForm.tsx
+++ src/components/SearchFrom/SearchForm.tsx
@@ -1,11 +1,34 @@
 import "./styles.css";
+import {useEffect, useState} from "react";
+import {useSearch} from "../SearchResults/SearchContext.ts";
 
 export function SearchForm() {
-  return (
-    <div className="searchForm">
-      <form>
-        <input type="text" value="Terry" />
-      </form>
-    </div>
-  );
+    const { setSearchTerm, setSearchResults } = useSearch();
+    const [inputValue, setInputValue] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`https://dummyjson.com/users/search?q=${inputValue}`);
+            const data = await response.json();
+            setSearchResults(data);
+        };
+
+        if (inputValue === '') {
+            fetchData();
+        } else {
+            const timeoutId = setTimeout(() => fetchData(), 300);
+            return () => clearTimeout(timeoutId);
+        }
+    }, [inputValue, setSearchResults]);
+
+    return (
+        <div className="searchForm">
+            <form>
+                <input type="text" value={inputValue} onChange={event => {
+                    setInputValue(event.target.value)
+                    setSearchTerm(event.target.value)
+                }}/>
+            </form>
+        </div>
+    );
 }
