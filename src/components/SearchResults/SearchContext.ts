0edfc4d638c3c9cbe260d5c4327e59edab0d52eb
Radmir Musabaev <musabaevradmir@gmail.com>
2024-03-07 23:02:45 +0400

    refactor: changed variables in context

--- src/components/SearchResults/SearchContext.ts
+++ src/components/SearchResults/SearchContext.ts
@@ -1,3 +1,19 @@
-import { createContext } from "react";
+import {createContext, useContext} from "react";
+import {ResponseEntity} from "../types/ResponseEntity.ts";
 
-export const SearchContext = createContext({ users: [] });
+interface SearchContextType {
+    searchTerm: string;
+    setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+    searchResults: ResponseEntity;
+    setSearchResults: React.Dispatch<React.SetStateAction<ResponseEntity>>;
+}
+
+export const SearchContext = createContext<SearchContextType | undefined>(undefined);
+
+export const useSearch = () => {
+    const context = useContext(SearchContext);
+    if (context === undefined) {
+        throw new Error('useSearch must be used within a SearchProvider');
+    }
+    return context;
+}
