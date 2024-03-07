1e98f6d115f1df166707a1a6b8aeeae27f85d156
Radmir Musabaev <musabaevradmir@gmail.com>
2024-03-07 23:04:59 +0400

    refactor: getting data from custom hook

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,17 +1,16 @@
-import { useContext } from "react";
-import { SearchContext } from "./SearchContext";
-import { UserCard } from "../UserCard/UserCard";
+import {useSearch} from "./SearchContext";
+import {UserCard} from "../UserCard/UserCard";
 
 import "./style.css";
 
 export function SearchResults() {
-  const { users } = useContext(SearchContext);
+    const {searchResults} = useSearch();
 
-  return (
-    <div className="usersList">
-      {users.map((user) => (
-        <UserCard {...user} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="usersList">
+            {searchResults.users.map(user =>
+                <UserCard user={user} />
+            )}
+        </div>
+    );
 }
